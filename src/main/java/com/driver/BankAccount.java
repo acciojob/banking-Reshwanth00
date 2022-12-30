653f497e0bc0f3e5b28ee88f346afca54837212d
Reshwanth00 <116064259+Reshwanth00@users.noreply.github.com>
2022-12-31 01:58:53 +0530

    Update BankAccount.java

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,5 +1,14 @@
 package com.driver;
-
+class Insufficient extends Exception{
+    Insufficient(){
+        super("Insufficient Balance");
+    }
+}
+class Invalid extends Exception{
+    Invalid(){
+        super("Account Number can not be generated");
+    }
+}
 public class BankAccount {
 
     private String name;
@@ -7,25 +16,70 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
-
+        this.minBalance=minBalance;
+        deposit(balance);
+        this.name=name;
     }
 
+    private String AccountNumber;
+    public void getNumber(int n, int sum, char out[],
+                                   int index)
+    {
+        // Base case
+        if (index > n || sum < 0)
+            return;
+
+        // If number becomes N-digit
+        if (index == n)
+        {
+            // if sum of its digits is equal to given sum,
+            // print it
+            if(sum == 0)
+            {
+                String p = new String(out);
+                this.AccountNumber = p;
+                return;
+            }
+            return;
+        }
+        for (int i = 0; i <= 9; i++){
+            out[index] = (char)(i + '0');
+            getNumber(n, sum - i, out, index + 1);
+        }
+    }
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
 
-        return null;
+        if(sum<0 || 9*digits<sum){
+            throw new Invalid();
+        }
+        else{
+            char [] out = new char[digits+1];
+            for(int i=1;i<=9;i++){
+                out [0] = (char)(i+'0');
+                getNumber(digits,sum-i,out,1);
+            }
+            return this.AccountNumber;
+
+        }
     }
 
     public void deposit(double amount) {
         //add amount to balance
-
+        this.balance += amount;
     }
 
     public void withdraw(double amount) throws Exception {
-        // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+        this.balance-=amount;
+        if(this.balance<this.minBalance){
+            throw new Insufficient();
+        }
+    }
 
+    public double getBalance() {
+        return balance;
     }
 
 }
